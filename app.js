e056c136c9fe8957cd386f7e7cad4d56f27e8530
José Echezuría <joseechezuria@Mac.attlocal.net>
2025-01-26 22:13:59 -0600

    sign up done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
+
 // Create __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
